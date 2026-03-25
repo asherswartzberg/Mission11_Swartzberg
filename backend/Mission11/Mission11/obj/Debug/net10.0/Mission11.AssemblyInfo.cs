08e234405e4fa7122b40c3bdbb50b3d9159b6509
asherswartzberg <agswartzberg@gmail.com>
2026-03-25 11:26:19 -0600

    delete weather

--- backend/Mission11/Mission11/obj/Debug/net10.0/Mission11.AssemblyInfo.cs
+++ backend/Mission11/Mission11/obj/Debug/net10.0/Mission11.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab920f4edd6896315981af7cfe1f841047bdb5c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
